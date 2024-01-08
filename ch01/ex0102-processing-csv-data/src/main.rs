c2567cdc124a8a144954e058aa3990a48acbb125
mindeleven <mindeleven@posteo.de>
2024-01-08 11:59:48 +0100

    New project for working along with chapter 1, example 2, processing CSV data

---
+++ ch01/ex0102-processing-csv-data/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // csv data example
+    let penguin_data ="\
+        common name,length (cm)
+        Little penguin,33
+        Yellow-eyed penguin,65
+        Fiordland penguin,60
+        Invalid,data
+    ";
+
+    
+    println!("Hello, world!");
+}
