d1d57a3a8824cb17cdba9b74a975a8082ee35e5a
mindeleven <mindeleven@posteo.de>
2024-01-08 12:35:29 +0100

    Reading the lines of the csv data, using enumerate() to collect the records into a vector

--- ch01/ex0102-processing-csv-data/src/main.rs
+++ ch01/ex0102-processing-csv-data/src/main.rs
@@ -8,6 +8,35 @@ fn main() {
         Invalid,data
     ";
 
+    // read the lines of the csv data
+    let records = penguin_data.lines();
     
+    // enumerate returns an iterator that yields the current count 
+    // and the element during iteration
+    // https://doc.rust-lang.org/std/iter/struct.Enumerate.html
+    for (i, record) in records.enumerate() {
+
+        // read the lines of the csv data but skip the header
+        // and skip the lines with only whitespace
+        if i == 0 || record.trim().len() == 0 {
+            continue;
+        }
+        
+        // collect the records into a vector
+        // vectors are collections that can dynamically expand
+        // the underscore (<_>) asks the compiler to infer the type of the elements
+        let fields: Vec<_> = record
+            // split record into substrings at the comma delimiter
+            .split(',')
+            // map makes use of a high order function
+            // trims the whitespace from every field
+            .map(|field| field.trim())
+            .collect();
+
+        println!("{:?}", fields);
+
+    }
+
+
     println!("Hello, world!");
 }
