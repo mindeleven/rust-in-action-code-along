87b75aa61c8d1a95ae07513301f1a04a643107ad
mindeleven <mindeleven@posteo.de>
2024-01-09 09:14:46 +0100

    New project for working along with chapter 1, example 3, dangling pointers

---
+++ ch01/ex0103-dangling-pointers/src/main.rs
@@ -0,0 +1,35 @@
+/// coding along with Rust in Action by Tim McNamara
+/// Chapter 1, Introducing Rust, example 3, dangling pointers
+/// code examples and comments are taken from the book
+/// 
+/// Rust programs are free from dangling pointers
+/// dangling pointers are live references to data 
+/// that has become invalid over the course of the program
+/// example: attempting to create a dangling pointer
+/// 
+/// derive(Debug) annotation allows the enum to be printed by the println! macro
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Cereal {
+    Barley, 
+    Millet, 
+    Rice, 
+    Rye, 
+    Spelt, 
+    Wheat,
+}
+fn main() {
+    // initializing an empty vector of Cereal
+    let mut grains: Vec<Cereal> = vec![];
+    // adding items to the grains vector
+    grains.push(Cereal::Barley);
+    grains.push(Cereal::Millet);
+    grains.push(Cereal::Rye);
+    grains.push(Cereal::Spelt);
+    // delete grains and its contents
+    drop(grains); // value moved here
+    // attempt to access the deleted value
+    // which will not compile
+    println!("{:?}", grains); // value borrowed here after move
+    // compiler not happy
+}
