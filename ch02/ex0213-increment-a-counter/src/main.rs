de3c8e71c3f939bac5f5c5e27da4d45d09c81854
mindeleven <mindeleven@posteo.de>
2024-01-14 14:45:54 +0100

    Added explanatory comment

--- ch02/ex0213-increment-a-counter/src/main.rs
+++ ch02/ex0213-increment-a-counter/src/main.rs
@@ -1,6 +1,6 @@
 /// coding along with Rust in Action by Tim McNamara
 /// Chapter 2, Language Foundations, Iteration, example 13, 
-/// incrementing a counter
+/// testing how fast your computer can increment a counter
 /// 
 /// code examples and comments are taken from the book
 /// 
