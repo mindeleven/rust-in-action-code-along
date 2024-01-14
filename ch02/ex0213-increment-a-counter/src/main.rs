6ce88fb8272191fba4fd89f52702309da22aedb9
mindeleven <mindeleven@posteo.de>
2024-01-14 13:13:19 +0100

    New project for coding along with Rust in Action, chapter 2, example 13, incrementing a counter

---
+++ ch02/ex0213-increment-a-counter/src/main.rs
@@ -0,0 +1,10 @@
+/// coding along with Rust in Action by Tim McNamara
+/// Chapter 2, Language Foundations, Iteration, example 13, 
+/// incrementing a counter
+/// 
+/// code examples and comments are taken from the book
+/// 
+
+fn main() {
+    println!("Hello, world!");
+}
