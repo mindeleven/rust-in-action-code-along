d790affac91fc0b8db4961e50cc40ae529c41870
mindeleven <mindeleven@posteo.de>
2024-01-21 14:55:49 +0100

    New project for coding along with Rust in Action by Tim McNamara, Chapter 2, Language Foundations, chapter 2.6, Defining Functions

---
+++ ch02/ex0217-fn-mandelbrot-set/src/main.rs
@@ -0,0 +1,11 @@
+/// coding along with Rust in Action by Tim McNamara
+/// Chapter 2, Language Foundations, chapter 2.6, defining functions, 
+/// 
+/// example: create a project that can render the Mandelbrot set
+/// 
+/// code examples and comments are taken from the book
+/// 
+
+fn main() {
+    println!("Hello, world!");
+}
