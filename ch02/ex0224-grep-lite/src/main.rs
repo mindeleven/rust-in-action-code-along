da7ac82523ec3b161f6e6edabf5c3312824fa804
mindeleven <mindeleven@posteo.de>
2024-01-22 18:47:12 +0100

    New project for coding along with Rust in Action chapter 2.9, creating grep-lite

---
+++ ch02/ex0224-grep-lite/src/main.rs
@@ -0,0 +1,20 @@
+/// coding along with Rust in Action by Tim McNamara
+/// Chapter 2, Language Foundations, chapter 2.9, creating grep-lite
+/// 
+/// code examples and comments are taken from the book
+/// 
+fn main() {
+    let search_term = "picture";
+    let quote = "\
+Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books. What do we seek through millions of pages?";
+    
+    // the lines method returns an iterator over quote
+    for line in quote.lines() {
+        // searching for text using method syntax
+        if line.contains(search_term) {
+            println!("{}", line);
+        }
+    }
+}
