e06e7d5516d38de6b4d0e491ad6099934f50bf58
mindeleven <mindeleven@posteo.de>
2024-01-22 17:46:28 +0100

    Elaborating the generic function example

--- ch02/ex0218-lifetime-annotations/src/main.rs
+++ ch02/ex0218-lifetime-annotations/src/main.rs
@@ -16,6 +16,9 @@
 /// -> when multiple references are accepted as arguments
 /// -> when a reference is returned from a function
 
+use std::ops::Add;
+use std::time::Duration;
+
 fn main() {
     let a = 10;
     let b = 20;
@@ -23,6 +26,15 @@ fn main() {
     // no lifetime notation is required when calling the function
     let res = add_with_lifetimes(&a, &b);
     println!("result: {}", res);
+
+    let floats = add(1.2, 3.4);
+    let ints = add(60, 10);
+    // calling add with duration values that represent a duration between two points in time
+    let duration = add(Duration::new(5, 0), Duration::new(10, 0));
+
+    println!("result floats: {}", floats);
+    println!("result ints: {}", ints);
+    println!("result duration: {:?}", duration);
 }
 
 #[allow(dead_code)]
@@ -44,6 +56,6 @@ fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32 {
 /// when creating generic functions that include an operator
 /// that operator's trait must be included as a trait bound
 #[allow(dead_code)]
-fn add<T: std::ops::Add<Output = T>>(i: T, j: T) -> T {
+fn add<T: Add<Output = T>>(i: T, j: T) -> T {
     i + j
 }
