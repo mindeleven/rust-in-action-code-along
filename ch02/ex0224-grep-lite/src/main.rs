5c4c03f068cf2b7eba62ce18dc85f63a4858820c
mindeleven <mindeleven@posteo.de>
2024-01-23 10:10:59 +0100

    Printing the line number with every result found

--- ch02/ex0224-grep-lite/src/main.rs
+++ ch02/ex0224-grep-lite/src/main.rs
@@ -9,12 +9,14 @@ fn main() {
 Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?";
+    let mut line_num: usize = 1;
     
     // the lines method returns an iterator over quote
     for line in quote.lines() {
         // searching for text using method syntax
         if line.contains(search_term) {
-            println!("{}", line);
+            println!("{}: {}", line_num, line);
         }
+        line_num += 1;
     }
 }
