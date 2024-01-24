4c623dbbe1acf9d42563ae75376c5e300ea861c6
mindeleven <mindeleven@posteo.de>
2024-01-24 12:23:30 +0100

    Rewriting the grep lite example with vectors

---
+++ ch02/ex0229-grep-lite-with-vectors/src/main.rs
@@ -0,0 +1,9 @@
+/// coding along with Rust in Action by Tim McNamara
+/// Chapter 2, Language Foundations, chapter 2.10, 
+/// creating grep-lite with vectors
+/// 
+/// code examples and comments are taken from the book
+/// 
+fn main() {
+    println!("Hello, world!");
+}
