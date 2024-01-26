a4e2030b2eeb4115cf6b0d4ee3470986a6c03bb5
mindeleven <mindeleven@posteo.de>
2024-01-26 10:23:13 +0100

    New project for working along with Rust in Action, chapter 2.13, reading from files

---
+++ ch02/ex0237-grep-lite-from-files/src/main.rs
@@ -0,0 +1,15 @@
+/// coding along with Rust in Action by Tim McNamara
+/// Chapter 2, Language Foundations, chapter 2.13, 
+/// reading from files
+/// 
+/// code examples and comments are taken from the book
+/// 
+/// task at hand: modifying the grep-lite tool from previous examples
+/// to be able to search within files
+/// 
+/// general pattern is to open a file object, the wrap it in BufReader
+/// 
+
+fn main() {
+    println!("Hello, world!");
+}
