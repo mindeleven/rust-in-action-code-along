22785c2b8b027e0412427a4946a4617a048ebd31
mindeleven <mindeleven@posteo.de>
2024-01-27 13:30:58 +0100

    Applying the methods we have so far to the struct

--- ch03/ex0306-struct-to-represent-files/src/main.rs
+++ ch03/ex0306-struct-to-represent-files/src/main.rs
@@ -24,9 +24,8 @@ struct File {
 /// Hostname is our newtype
 struct Hostname(String);
 
-
 /// creating a type alias
-type FileType = String;
+// type FileType = String;
 
 fn open(f: &mut File) -> bool {
     true
@@ -57,25 +56,38 @@ fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
 #[allow(unused_variables)]
 fn main() {
     // File "inherits" all of String’s methods
-    let mut f1 = FileType::from("f1.txt");
-    open(&mut f1);
+    // let mut f1 = FileType::from("f1.txt");
+    // open(&mut f1);
     // read(&mut f1, vec![]);
-    close(&mut f1);
+    // close(&mut f1);
 
     // creating a file from the File struct
     // creating a first instance of File
-    let f2 = File {
+    let mut f2 = File {
         // String::from allows owned strings to be generated from string literals
         name: String::from("f2.txt"),
         // using the vec! macro to simulate an empty file
-        data: Vec::new(),
+        data: vec![114, 117, 115, 116, 33],
     };
+    
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f2);
+    let f2_length = read(&f2, &mut buffer);
+    close(&mut f2);
+
+    // convert Vec<u8> to String
+    // any bytes that are not valid UTF-8 are replaced with ?
+    let text = String::from_utf8_lossy(&buffer);
+
     // accessing fields uses the dot operator
     let f2_name = &f2.name;
     let f2_length = &f2.data.len();
 
     println!("{:?}", f2);
     println!("{} is {} bytes long", f2_name, f2_length);
+    // view the bytes 114, 117, 115, 116 & 33 as an actual word
+    println!("{}", text);
 
     // creating an ordinary string to compare it withn the Hostname newtype
     let ordinary_string = String::from("localhost");
