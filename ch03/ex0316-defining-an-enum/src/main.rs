4b59436f24d042ba11be3c3bd474ae0c31709774
mindeleven <mindeleven@posteo.de>
2024-01-29 12:42:02 +0100

    Elaborated a bit on the example snippet and added a few comments

--- ch03/ex0316-defining-an-enum/src/main.rs
+++ ch03/ex0316-defining-an-enum/src/main.rs
@@ -4,22 +4,52 @@
 /// code examples and comments are taken from the book
 /// 
 
+/// enabling the enum to be printed to the screen
 #[derive(Debug)]
 enum Event {
+    // creating three variants of Event
+    // including one value for unrecognized events
     Update,
     Delete,
     Unknown
 }
 
+// defining a Message type as a convenient name for String for use in this crate’s context
 type Message = String;
 
-fn parse_log(line: &'static String) -> (Event, Message) {
-    
+// function for parsing a line and converting it into semi-structured data
+fn parse_log(line: &'static str) -> (Event, Message) {
+    // line.splitn() returns an iterator
+    // collect() consumes this iterator and returns Vec<T>
     let parts: Vec<&str> = line.splitn(2, ' ').collect();
-
-    unimplemented!()
+    
+    // line.splitn() is supposed to split log into two parts
+    // return an error if this doesn't happen
+    if parts.len() == 1 {
+        return (Event::Unknown, String::from(line))
+    }
+    
+    // assign each part to a variable for future use
+    let event = parts[0];
+    let rest = String::from(parts[1]);
+    
+    // now let's see what event we have
+    match event {
+        // return structured data if we match a known event
+        "update" | "UPDATE" => (Event::Update, rest),
+        "delete" | "DELETE" => (Event::Delete, rest),
+        // return the whole line if we don’t recognize the event type
+        _ => (Event::Unknown, String::from(line))
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let log = "BEGIN Transaction XK342
+UPDATE 234:LS/32231 {\"price\": 31.00} -> {\"price\": 40.00}
+DELETE 342:LO/22111";
+
+    for line in log.lines() {
+        let parse_result = parse_log(line);
+        println!("{:?}", parse_result);
+    }
 }
