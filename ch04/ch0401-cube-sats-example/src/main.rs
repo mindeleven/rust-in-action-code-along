4f41a335c44d95f69dd54bc0f146e41189242f85
mindeleven <mindeleven@posteo.de>
2024-02-09 13:03:09 +0100

    Printing out the state of the sattelites

--- ch04/ch0401-cube-sats-example/src/main.rs
+++ ch04/ch0401-cube-sats-example/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_variables)]
 /// coding along with Rust in Action by Tim McNamara
 /// Chapter 4, Lifetimes, Ownership and Borrowing, chapter 4.1, 
 /// 4.1 “Implementing” a Mock CubeSat Ground Station
@@ -11,6 +12,10 @@ enum StatusMessage {
     Ok,
 }
 
+fn check_status(sat_id: u64) -> StatusMessage {
+    StatusMessage::Ok
+}
+
 fn main() {
     // model with three CubeSats
     let sat_a = 0;
@@ -18,11 +23,14 @@ fn main() {
     let sat_c = 2;
 
     let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 
-    print!("a: {:?}", a_status);
+    // "waiting" ...
+    let a_status: StatusMessage = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 
-}
-
-fn check_status(sat_id: u64) -> StatusMessage {
-    StatusMessage::Ok
 }
