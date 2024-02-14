84b89cbe318965a26a014c54a5ba33547307f740
mindeleven <mindeleven@posteo.de>
2024-02-14 13:24:41 +0100

    Implementing the Copy trait with derive and manually

--- ch04/ch0420-cube-sats-copy-clone-version/src/main.rs
+++ ch04/ch0420-cube-sats-copy-clone-version/src/main.rs
@@ -5,8 +5,23 @@
 /// 
 /// code examples and comments are taken from the book
 /// 
+/// an alternative to Having a single owner is to simply copy values
+/// primitive types, such as integers, are a good example of that
+///
+/// types can opt into two modes of being copied: Clone and Copy
+/// => Copy acts implicitly whenever ownership would otherwise be moved
+///    the bits of object a are replicated to create object b
+/// => Clone acts explicitly
+///    types that implement Clone have a .clone() method that is permitted 
+///    to do whatever it needs to create a new type
+/// 
+///    to implement Copy, your types must consist of types that themselves implement Copy
+/// 
 
-#[derive(Debug)]
+/// struct consists of types that contain types that themselves implement Copy
+/// so implementing copy for the type isn't a problem
+/// adding #[derive(Copy)] tells the compiler to add an implementation itself
+#[derive(Copy, Clone, Debug)]
 struct CubeSat {
     id: u64,
 }
@@ -14,6 +29,15 @@ struct CubeSat {
 #[derive(Debug)]
 enum StatusMessage {
     Ok,
+}
+
+/// implementing the Copy trait manually for status message
+impl Copy for StatusMessage { }
+
+impl Clone for StatusMessage {
+    fn clone(&self) -> Self {
+        *self
+    }
 }
 
 fn check_status(sat_id: CubeSat) -> StatusMessage {
